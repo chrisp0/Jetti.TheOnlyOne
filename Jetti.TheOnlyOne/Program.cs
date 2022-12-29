9e655e09b0377dcd9b2cf9b061b1b5e1b4206e1c
chrisp0 <christian.poschmann@yahoo.de>
2022-12-29 19:50:53 +0100

    Projektdateien hinzufügen.

---
+++ Jetti.TheOnlyOne/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Jetti.TheOnlyOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Jetti!");
+            
+        }
+    }
+}
