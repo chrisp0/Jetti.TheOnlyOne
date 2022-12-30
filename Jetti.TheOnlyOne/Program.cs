119855faf85df3d8d0f83a5fbe17b19627bff23d
chrisp0 <christian.poschmann@yahoo.de>
2022-12-30 15:50:51 +0100

    knowingly failure

--- Jetti.TheOnlyOne/Program.cs
+++ Jetti.TheOnlyOne/Program.cs
@@ -6,7 +6,7 @@ namespace Jetti.TheOnlyOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Jetti!");
+            Console.WriteLine("Hello, Jetti!"
             
         }
     }
